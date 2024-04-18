f93c787a5a18ef004747834a44745647277b6d2a
Captain-DAB <115474437+Captain-DAB@users.noreply.github.com>
2024-04-18 11:59:10 +0100

    Going through basics

---
+++ src/welcome/Welcome.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package welcome;
+
+/**
+ *
+ * @author USER
+ */
+public class Welcome {
+
+    public static void main(String[] args) {
+        //display welcome message to the console
+        System.out.println("Welcome to Java");
+        System.out.println("Programming is fun");
+        System.out.println("Fundamentals first");
+        System.out.println("Problem driven");
+        //Compute Expression
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+}
+        //Show syntax Error or Compile Error
+//Error that occurs due to code construction, mistyping or omitting some necessary punctuation
+//public class ShowSyntaxErrors{
+//    public static main(String[] args){
+//        System.out.println("Welcome to Java);
+//    }
+//}
+
+        //Show Runtime Error
+//it makes a program terminate abnormally, runtime error occurs if the environment detects an operation that is impossible to carry out.
+//public class ShowRuntimeErrors{
+//    public static void main(String[] args){
+//        System.out.println(1 / 0);
+//    }
+//}
+        //Logic Errors
+//Logic errors occur when a program does not perform the way it was intended to.
+//public class ShowLogicErrors {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Celsius 35 is Fahrenheit degree ");
+//        System.out.println((9 / 5) * 35 + 32); This is corrected as System.out.println((9.0 / 5) * 35 + 32);
+//    }
+//}
+
+
+//In general, syntax errors are easy to find and easy to correct because the compiler gives 
+//indications as to where the errors came from and why they are wrong. Runtime errors are not 
+//difficult to find, either, since the reasons and locations for the errors are displayed on the console 
+//when the program aborts. Finding logic errors, on the other hand, can be very challenging.
