75f54141c70fe6c9d3edb0c0f569c50fe45a8e2a
Captain-DAB <115474437+Captain-DAB@users.noreply.github.com>
2024-04-18 17:32:43 +0100

    Updated Basics learning

--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -4,6 +4,8 @@
  */
 package welcome;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USER
@@ -18,6 +20,72 @@ public class Welcome {
         System.out.println("Problem driven");
         //Compute Expression
         System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+        
+        //Compute Area of a Circle
+//        double radius;
+        double area;
+        final double PI = 3.14159;
+        
+        Scanner input = new Scanner(System.in);
+        //Compute Area with console Input
+        System.out.println("Enter your radius: ");
+        double radius = input.nextDouble();
+        
+//        radius = 20; // radius is now 20
+        // Compute area
+        area = radius * radius * PI;
+        // Display results
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+        
+        //Compute Average of three numbers
+        System.out.println("Enter first number: ");
+        double num1 = input.nextDouble();
+        System.out.println("Enter second number: ");
+        double num2 = input.nextDouble();
+        System.out.println("Enter third number: ");
+        double num3 = input.nextDouble();
+        
+        double average = ((num1 + num2 + num3) / 3);
+        System.out.println("The average of the three numbers is " + average);
+        
+        double i = 50.0;
+        double k = i + 50.0;
+        double j = k + 1;
+        System.out.println("j is " + j + " and k is " + k);
+        
+        //Assignment Expression or Statements
+        //An expression represents a computation involving values, variables, and operators that, taking them together, evaluates to a value
+
+//        int y = 1; // Assign 1 to variable y
+//        double radius = 1.0; // Assign 1.0 to variable radius
+//        int x = 5 * (3 / 2); // Assign the value of the expression to x
+//        x = y + 1; // Assign the addition of y and 1 to x
+//        double area = radius * radius * 3.14159; // Compute area
+
+        //1 = x is wrong
+        int a, b, c;
+        a = b = c = 2;
+        System.out.println(a + " " + b + " " + c);
+        
+        int x;
+        System.out.println(x = 2);
+        System.out.println(x);
+        
+//        Translate the following algorithm into Java code:
+//        Step 1: Declare a double variable named miles with initial value 100.
+//        Step 2: Declare a double constant named KILOMETERS_PER_MILE with value 
+//        1.609.
+//        Step 3: Declare a double variable named kilometers, multiply miles and 
+//        KILOMETERS_PER_MILE, and assign the result to kilometers.
+//        Step 4: Display kilometers to the console.
+//         What is kilometers after Step 4?
+
+        double miles = 100;
+        final double KILOMETERS_PER_MILES = 1.609;
+        double kilometers = miles * KILOMETERS_PER_MILES;
+        System.out.println("Kilometers: " + kilometers);
+        
+        
     }
 }
         //Show syntax Error or Compile Error
